43091581755133dd55288d60b4d687a67256ef9d
techdeus <sullivan.marlon@gmail.com>
2018-10-02 16:29:46 -0700

    Fix syntax errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,29 +2,74 @@ import VideoList from './VideoList.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 import VideoPlayer from './VideoPlayer.js';
 import Search from './Search.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+    
+  }
+
+  switchVideo(video) {
+    console.log('click');      
+    
+    this.setState({currentVideo: video});
+  }
+  
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div>
+              <VideoPlayer video={this.state.currentVideo}/>
+            </div>
+          </div>
+          <div className="col-md-5">
+            <div>
+              <VideoList videos={this.state.videos} 
+                switchVideo={this.switchVideo.bind(this)}
+              />          
+            </div>
+          </div>
+        </div>
+      </div>
+    ); 
+  }
+}
+
+export default App;
+
 //convert to ES6 class
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
+// var App = () => (
+//   <div>
+//     <nav className="navbar">
+//       <div className="col-md-6 offset-md-3">
         
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div>
-          <VideoPlayer video={exampleVideoData[0]}/>
-        </div>
-      </div>
-      <div className="col-md-5">
-        <div>
-          <VideoList videos={exampleVideoData}/>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+//       </div>
+//     </nav>
+//     <div className="row">
+//       <div className="col-md-7">
+//         <div>
+//           <VideoPlayer video={exampleVideoData[0]}/>
+//         </div>
+//       </div>
+//       <div className="col-md-5">
+//         <div>
+//           <VideoList videos={exampleVideoData}/>
+//         </div>
+//       </div>
+//     </div>
+//   </div>
+// );
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-export default App;
