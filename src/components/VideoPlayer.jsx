4ba82cbc472ff1dcc718c2b52a148a55e8285ccc
techdeus <sullivan.marlon@gmail.com>
2018-10-02 11:13:56 -0700

    Finished video player and render

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,18 +1,16 @@
-var VideoPlayer = (props) => {
-  const video = props.videoplayer[0];
 
-  return (
-    <div className="video-player">
-      <div className="embed-responsive embed-responsive-16by9">
-        <iframe className="embed-responsive-item" src={"https://www.youtube.com/embed/" + props.videoplayer[0].id.videoId} allowFullScreen></iframe>
-      </div>
-      <div className="video-player-details">
-        <h3>{props.videoplayer[0].snippet.title}</h3>
-        <div>{props.videoplayer[0].snippet.description}</div>
-      </div>
+var VideoPlayer = (props) => (
+
+  <div className="video-player">
+    <div className="embed-responsive embed-responsive-16by9">
+      <iframe className="embed-responsive-item" src={"https://www.youtube.com/embed/" + props.video.id.videoId} allowFullScreen></iframe>
     </div>
-  );
-};
+    <div className="video-player-details">
+      <h3>{props.video.snippet.title}</h3>
+      <div>{props.video.snippet.description}</div>
+    </div>
+  </div>
+);
 
 
 // var VideoPlayer = (props) => (
